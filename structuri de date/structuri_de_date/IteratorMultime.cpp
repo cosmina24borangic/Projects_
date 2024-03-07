36312f13d20c332b404cac3ef53fa41200bea500
cosmina24borangic <162466618+cosmina24borangic@users.noreply.github.com>
2024-03-07 15:21:34 +0200

    add projects

---
+++ structuri de date/structuri_de_date/IteratorMultime.cpp
@@ -0,0 +1,25 @@
+#include "IteratorMultime.h"
+#include "Multime.h"
+
+
+IteratorMultime::IteratorMultime(const Multime& m) : mult(m) {
+	/* de adaugat */
+}
+
+TElem IteratorMultime::element() const {
+	/* de adaugat */
+	return -1;
+}
+
+bool IteratorMultime::valid() const {
+	/* de adaugat */
+	return false;
+}
+
+void IteratorMultime::urmator() {
+	/* de adaugat */
+}
+
+void IteratorMultime::prim() {
+	/* de adaugat */
+}
